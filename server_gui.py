591b0658d15024c6f67de4b4e434573d0b4c8b57
Etery89 <shadowx110@yandex.ru>
2021-12-27 21:53:08 +0300

    added config window class

--- server_gui.py
+++ server_gui.py
@@ -1,6 +1,7 @@
 import sys
 import os
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QAction, QApplication, QLabel, QTableView, QDialog
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QAction, QApplication, QLabel, QTableView, QDialog, QLineEdit, \
+    QFileDialog
 
 
 class MainWindow(QMainWindow):
@@ -65,10 +66,79 @@ class HistoryWindow(QDialog):
         self.show()
 
 
+class ConfigWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setFixedSize(395, 270)
+        self.setWindowTitle('Настройки сервера')
+
+        self.db_path_label = QLabel('Выбрать файл базы данных: ', self)
+        self.db_path_label.move(10, 10)
+        self.db_path_label.setFixedSize(240, 15)
+
+        self.db_path = QLineEdit(self)
+        self.db_path.setFixedSize(250, 20)
+        self.db_path.move(10, 30)
+        self.db_path.setReadOnly(True)
+
+        self.db_path_select = QPushButton('Обзор...', self)
+        self.db_path_select.move(285, 28)
+
+        def open_file_dialog():
+            global dialog
+            dialog = QFileDialog(self)
+            path = dialog.getExistingDirectory()
+            path = path.replace('/', '\\')
+            self.db_path.insert(path)
+
+        self.db_path_select.clicked.connect(open_file_dialog)
+
+        self.db_file_label = QLabel('Название файла базы данных: ', self)
+        self.db_file_label.move(10, 68)
+        self.db_file_label.setFixedSize(220, 15)
+
+        self.db_file = QLineEdit(self)
+        self.db_file.move(230, 66)
+        self.db_file.setFixedSize(150, 20)
+
+        self.port_label = QLabel('Номер порта для соединений:', self)
+        self.port_label.move(10, 108)
+        self.port_label.setFixedSize(220, 15)
+
+        self.port = QLineEdit(self)
+        self.port.move(230, 108)
+        self.port.setFixedSize(150, 20)
+
+        self.ip_label = QLabel('IP для приёма соединения:', self)
+        self.ip_label.move(10, 148)
+        self.ip_label.setFixedSize(220, 15)
+
+        self.ip_label_note = QLabel('(оставьте это поле пустым, чтобы\nпринимать соединения с любых адресов)', self)
+        self.ip_label_note.move(10, 175)
+        self.ip_label_note.setFixedSize(500, 30)
+
+        self.ip = QLineEdit(self)
+        self.ip.move(230, 148)
+        self.ip.setFixedSize(150, 20)
+
+        self.save_btn = QPushButton('Сохранить' , self)
+        self.save_btn.move(80, 225)
+
+        self.close_button = QPushButton('Закрыть', self)
+        self.close_button.move(225, 225)
+        self.close_button.clicked.connect(self.close)
+
+        self.show()
+
+
 if __name__ == "__main__":
     my_app = QApplication(sys.argv)
-    win = MainWindow()
+    # win = MainWindow()
     # his = HistoryWindow()
+    config = ConfigWindow()
     my_app.exec_()
 
 
